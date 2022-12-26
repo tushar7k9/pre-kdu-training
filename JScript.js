78add50592dbf640a4ccbd83ee1fa8253d4b7aac
Tushar <tushar7k9@gmail.com>
2022-12-26 12:05:07 +0530

    Exercise 1: Javascript

---
+++ JScript.js
@@ -0,0 +1,21 @@
+const counter = document.querySelector('#count')
+const btns = document.querySelectorAll('.btn')
+
+let count = 0;
+
+btns.forEach((btn) => {
+    btn.addEventListener('click',(e) => {
+        const styles = e.currentTarget.classList
+
+        if(styles.contains('increase'))
+        {
+            count++;
+        }
+        else if(styles.contains('decrease'))
+        {
+            count--;
+        }
+
+        counter.textContent = count
+    })
+})
